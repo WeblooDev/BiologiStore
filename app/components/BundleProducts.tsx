758d87c44cf442a4610560f603fa4437dc81fa28
nabeff <neffina8@gmail.com>
2025-07-09 21:02:48 +0100

    phase 11

--- app/components/BundleProducts.tsx
+++ app/components/BundleProducts.tsx
@@ -6,6 +6,7 @@ type Props = {
   products: Product[];
 };
 
+
 function chunkArray<T>(arr: T[], size: number): T[][] {
   return Array.from({length: Math.ceil(arr.length / size)}, (_, i) =>
     arr.slice(i * size, i * size + size),
